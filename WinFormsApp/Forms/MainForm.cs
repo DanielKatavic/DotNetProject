16ac4d86df6838b36859ada18a15381ee0354457
Daniel Katavić <dkatav2@racunarstvo.hr>
2022-05-09 00:46:53 +0200

    Added PrintManager

--- WinFormsApp/Forms/MainForm.cs
+++ WinFormsApp/Forms/MainForm.cs
@@ -188,75 +188,19 @@ namespace WinFormsApp.Forms
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
             => PlayerManager.SaveFavouritePlayers();
 
-        private static void PrintPlayers(Control parent, PrintPageEventArgs e)
-        {
-            int x = 10, y = 10;
-
-            for (int i = 0; i < parent.Controls.Count; i++)
-            {
-                var child = parent.Controls[i];
-                Bitmap bmp = new(child.Size.Width, child.Size.Height);
-                parent.Controls[i].DrawToBitmap(bmp, new Rectangle
-                {
-                    X = 0,
-                    Y = 0,
-                    Width = bmp.Width,
-                    Height = bmp.Height
-                });
-                if (i == 4)
-                {
-                    x = child.Size.Width + 30;
-                    y = 10;
-                }
-                if (i == 8)
-                {
-                    x = (child.Size.Width + 30) * 2;
-                    y = 10;
-                }
-                e.Graphics?.DrawImage(bmp, e.MarginBounds.X + x, e.MarginBounds.Y + y);
-                y += child.Size.Height;
-            }
-        }
-
-        private static void PrintMatches(Control parent, PrintPageEventArgs e)
-        {
-            int x = 0, y = 0;
-
-            for (int i = 0; i < parent.Controls.Count; i++)
-            {
-                var child = parent.Controls[i];
-                Bitmap bmp = new(child.Size.Width, child.Size.Height);
-                parent.Controls[i].DrawToBitmap(bmp, new Rectangle
-                {
-                    X = 0,
-                    Y = 0,
-                    Width = bmp.Width,
-                    Height = bmp.Height
-                });
-                if (i == 4)
-                {
-                    x = 0;
-                    y = 0;
-                }
-                e.Graphics?.DrawImage(bmp, x, y);
-                y += child.Size.Height;
-            }
-
-        }
-
         private void YellowCards_PrintPage(object sender, PrintPageEventArgs e)
         {
-            PrintPlayers(flpYellowCards, e);
+            PrintManager.PrintPlayers(flpYellowCards, e);
         }
 
         private void Goals_PrintPage(object sender, PrintPageEventArgs e)
         {
-            PrintPlayers(flpGoals, e);
+            PrintManager.PrintPlayers(flpGoals, e);
         }
 
         private void Matches_PrintPage(object sender, PrintPageEventArgs e)
         {
-            PrintMatches(flpMatches, e);
+            PrintManager.PrintMatches(flpMatches, e);
         }
 
         private void Print_Click(object sender, EventArgs e)
---
+++ WinFormsApp/PrintManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Drawing.Printing;
+
+namespace WinFormsApp
+{
+    internal class PrintManager
+    {
+        internal static void PrintPlayers(Control parent, PrintPageEventArgs e)
+        {
+            int x = 10, y = 10;
+
+            for (int i = 0; i < parent.Controls.Count; i++)
+            {
+                var child = parent.Controls[i];
+                Bitmap bmp = new(child.Size.Width, child.Size.Height);
+                parent.Controls[i].DrawToBitmap(bmp, new Rectangle
+                {
+                    X = 0,
+                    Y = 0,
+                    Width = bmp.Width,
+                    Height = bmp.Height
+                });
+                if (i == 4)
+                {
+                    x = child.Size.Width + 30;
+                    y = 10;
+                }
+                if (i == 8)
+                {
+                    x = (child.Size.Width + 30) * 2;
+                    y = 10;
+                }
+                e.Graphics?.DrawImage(bmp, e.MarginBounds.X + x, e.MarginBounds.Y + y);
+                y += child.Size.Height;
+            }
+        }
+
+        internal static void PrintMatches(Control parent, PrintPageEventArgs e)
+        {
+            int x = 0, y = 0;
+
+            for (int i = 0; i < parent.Controls.Count; i++)
+            {
+                var child = parent.Controls[i];
+                Bitmap bmp = new(child.Size.Width, child.Size.Height);
+                parent.Controls[i].DrawToBitmap(bmp, new Rectangle
+                {
+                    X = 0,
+                    Y = 0,
+                    Width = bmp.Width,
+                    Height = bmp.Height
+                });
+                if (i == 4)
+                {
+                    x = 0;
+                    y = 0;
+                }
+                e.Graphics?.DrawImage(bmp, x, y);
+                y += child.Size.Height;
+            }
+        }
+    }
+}
